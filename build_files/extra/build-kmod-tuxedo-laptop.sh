a09da5033a6a8daa485024b3a6a3428f43c05aff
Gerry Agbobada <git@gagbo.net>
2025-05-28 18:10:27 +0200

    Add tuxedo-drivers kmod

---
+++ build_files/extra/build-kmod-tuxedo-laptop.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Source https://github.com/ublue-os/akmods/pull/329/files
+
+set -oeux pipefail
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+if [[ "${RELEASE}" -ge 43 ]]; then
+    COPR_RELEASE="rawhide"
+else
+    COPR_RELEASE="${RELEASE}"
+fi
+
+curl -LsSf -o /etc/yum.repos.d/_copr_gladion136-tuxedo-drivers-kmod.repo "https://copr.fedorainfracloud.org/coprs/gladion136/tuxedo-drivers-kmod/repo/fedora-${COPR_RELEASE}/gladion136-tuxedo-drivers-kmod-fedora-${COPR_RELEASE}.repo"
+
+dnf install -y \
+    "akmod-tuxedo-drivers-*.fc${RELEASE}.${ARCH}"
+
+akmods --force --kernels "${KERNEL}" --kmod tuxedo-drivers-kmod
+
+for file in /usr/lib/modules/${KERNEL}/extra/tuxedo-drivers/*.ko.xz; do
+    modinfo "$file" > /dev/null \
+    || (find /var/cache/akmods/tuxedo-drivers/ -name \*.log -print -exec cat {} \; && exit 1)
+done
+
+rm -f /etc/yum.repos.d/_copr_gladion136-tuxedo-drivers-kmod.repo
