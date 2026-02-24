64b284374c3d1398465edc1c273ba12c2e36de9a
Gerry Agbobada <gerry@tsuga.com>
2026-02-24 16:51:36 +0100

    Add Screenshot pipeline

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,39 +6,39 @@ set -ouex pipefail
 
 ## Set a group for github actions logs
 function echo_group() {
-    set +x
-    local WHAT
-    WHAT="$(
-        basename "$1" .sh |
-            tr "-" " " |
-            tr "_" " "
-    )"
-    echo "::group:: == ${WHAT^^} =="
-    set -x
-    "$1"
-    set +x
-    echo "::endgroup::"
-    set -x
+	set +x
+	local WHAT
+	WHAT="$(
+		basename "$1" .sh |
+			tr "-" " " |
+			tr "_" " "
+	)"
+	echo "::group:: == ${WHAT^^} =="
+	set -x
+	"$1"
+	set +x
+	echo "::endgroup::"
+	set -x
 }
 
 log() {
-  echo "== $* =="
+	echo "== $* =="
 }
 
 log "Starting Barbatos build process - Inspired by VeneOS, AmyOS and m2os"
 
 case "$BASE_IMAGE" in
-*"/bazzite"*|*"/aurora"*)
-    # Removing the Tuxedo step until we figure out the build error on Fedora 43.
-    # Example: https://github.com/gagbo/barbatos/actions/runs/18904270020/job/53974623145
-    # echo_group /ctx/tuxedo.sh
-    echo_group /ctx/desktop-packages.sh
-    echo_group /ctx/just-files.sh
-    echo_group /ctx/desktop-defaults.sh
-    ;;
+*"/bazzite"* | *"/aurora"*)
+	# Removing the Tuxedo step until we figure out the build error on Fedora 43.
+	# Example: https://github.com/gagbo/barbatos/actions/runs/18904270020/job/53974623145
+	# echo_group /ctx/tuxedo.sh
+	echo_group /ctx/desktop-packages.sh
+	echo_group /ctx/satty.sh
+	echo_group /ctx/just-files.sh
+	echo_group /ctx/desktop-defaults.sh
+	;;
 *"/ucore"*) ;;
 esac
-
 
 log "Post build cleanup"
 echo_group /ctx/cleanup.sh
